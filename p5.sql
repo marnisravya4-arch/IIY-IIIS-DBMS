1ef8348a54a2251298f720094446c1d44cf147e6
marnisravya4-arch <marnisravya4@gmail.com>
2025-10-21 03:22:42 +0530

    Add files via upload

---
+++ p5.sql
@@ -0,0 +1,16 @@
+create database p_5;
+use p_5;
+Drop view viewname;
+create view my_view as select rollno, name from source_table;
+
+Display Views and Tables in your login
+select * from tab;
+Inserting a row in view 
+insert into myview values(506,'prathisha'); 
+1 row(s) inserted
+select * from myview; 
+select * from st1;
+delete from myview where rollno=506; 1
+row(s) deleted 
+select * from myview;
+select * from st1;
